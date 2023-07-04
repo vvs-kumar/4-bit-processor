6414e587ca01bd576bc463871c982a8910e9b02b
Santhosh Kumar V V <53949655+vvs-kumar@users.noreply.github.com>
2023-07-04 19:52:58 +0800

    Update module names
    Updated all modules name for tool flow.

--- all.v
+++ all.v
@@ -1,1 +1,12 @@
+`include "adder_1_bit.v"
+`include "sub_1bit.v"
+`include "adder.v"
+`include "subtractor.v"
+`include "program_memory.v"
+`include "data_memory.v"
+`include "accumulator.v"
+`include "alu.v"
+`include "instruction_decoder.v"
 `include "control_unit.v"
+
+//This is a file for tool DV, which can combine all modules and places heirarchically.
