2c1ff9965f870945a80dff5dcc558465f700f4db
Santhosh Kumar V V <53949655+vvs-kumar@users.noreply.github.com>
2023-07-04 18:53:24 +0800

    Update accumulator.v

--- accumulator.v
+++ accumulator.v
@@ -1,6 +1,6 @@
 module accumulator(input clk,input [3:0] in, input acc_en, input reset, output reg [3:0] acc_out);
 
-    always @(posedge clk)
+always @(posedge clk)
     begin
         if (reset) // reset the accumulator to 0
             acc_out <= 4'b0000;
